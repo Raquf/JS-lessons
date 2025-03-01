ddab8cd4366b1fc0e533e589424cde74e6598066
Raquf Mehdiyev <raguf1989@gmail.com>
2025-03-01 21:07:59 +0400

    Variables: "var, let, const"

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-console.log("External file");
+//--------------------- 1th JS lesson ---------------------//
+
+// console.log("External file");
 
 //--------------------- Add error ---------------------//
 
-console.error("This file has some problems!!!");
+//console.error("This file has some problems!!!");
 
 //--------------------- Add grouping ---------------------//
-console.group("Group of logs");
+//console.group("Group of logs");
 
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4");
-console.log("5");
+//console.log("1");
+//console.log("2");
+//console.log("3");
+//console.log("4");
+//console.log("5");
 
-console.groupEnd();
+//console.groupEnd();
 
 //--------------------- Add table ---------------------//
 
-console.table({ name: "Ramey", age: 35 });
+//console.table({ name: "Ramey", age: 35 });
 
 //--------------------- Add styles ---------------------//
 
-const styles =
-  "background-color:lightblue; color:black; margin:20px; padding:15px";
+//const styles =
+//  "background-color:lightblue; color:black; margin:20px; padding:15px";
 
-console.log("%cStart coding, now!", styles);
+//console.log("%cStart coding, now!", styles);
 //--------------------- Add warning ---------------------//
 
-console.warn("Check this file!!!");
+//console.warn("Check this file!!!");
+
+//--------------------- 2th JS lesson ---------------------//
+
+//--------------------- Add variables ---------------------//
+
+//-Declaration (let info);
+//-Assignment (info = "work");
+
+//let info = "work";
+
+/* let a = 1;
+b = 2;
+c = 3;
+
+console.log(info); */
+
+const firstName = "Ramey";
+const age = 35;
+
+console.log(typeof firstName, typeof age);
