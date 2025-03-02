92a7a8d800d2b1736e1f9fd20225765db51edfd0
Raquf Mehdiyev <raguf1989@gmail.com>
2025-03-02 21:54:50 +0400

    Data types

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
 
 //console.warn("Check this file!!!");
 
-//--------------------- 2th JS lesson ---------------------//
+//--------------------- 2th JS lesson "Variables" ---------------------//
 
 //--------------------- Add variables ---------------------//
 
@@ -46,7 +46,90 @@ c = 3;
 
 console.log(info); */
 
-const firstName = "Ramey";
+// const firstName = "Ramey";
+// const age = 35;
+
+// console.log(typeof firstName, typeof age);
+
+//--------------------- 3th JS lesson "Data types" ---------------------//
+
+// 8 data types: number, bigInt, string, boolean, underfined, null, object, symbol
+
+//--------------------- Number ---------------------//
+
 const age = 35;
 
-console.log(typeof firstName, typeof age);
+console.log(age);
+console.log(typeof age);
+
+//--------------------- String ---------------------//
+
+const firstName = "Ramey";
+
+console.log(firstName);
+console.log(typeof firstName);
+
+//--------------------- BigInt ---------------------//
+
+const bigNumber = 12345678654323456n;
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+//--------------------- Boolean ---------------------//
+
+const isActive = true;
+const isConfirned = false;
+
+console.log(isActive, isConfirned);
+console.log(typeof isActive, typeof isConfirned);
+
+//--------------------- Null ---------------------//
+
+const city = null;
+
+console.log(city);
+console.log(typeof city);
+
+//--------------------- Underfined ---------------------//
+
+let isLoading;
+
+console.log(isLoading);
+console.log(typeof isLoading);
+
+//--------------------- Symbol ---------------------//
+
+const id = Symbol("123");
+
+console.log(id);
+console.log(typeof id);
+
+//--------------------- Reference types ---------------------//
+
+//--------------------- Object ---------------------//
+
+const person = {
+  firstName: "Ramey",
+  lastName: "Mehdiyev",
+  age: 35,
+};
+
+console.log(person);
+console.log(typeof person);
+
+//--------------------- Array ---------------------//
+
+const numArr = [1, 2, 3, 4, 5];
+
+console.log(numArr);
+console.log(typeof numArr);
+
+//--------------------- Function ---------------------//
+
+function info() {
+  return "Hello World!";
+}
+
+console.log(info);
+console.log(typeof info);
